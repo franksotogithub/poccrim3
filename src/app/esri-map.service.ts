d769f070cce96e91f90825f02b15d7451b8fc922
fsoto <franksoto2012@gmail.com>
2019-05-30 12:26:36 -0500

    fix servicio poccrim v1

--- src/app/esri-map.service.ts
+++ src/app/esri-map.service.ts
@@ -1,16 +1,15 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
-import { HttpClient, HttpHeaders, HttpParams,HttpErrorResponse } from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams, HttpErrorResponse} from '@angular/common/http';
 
-import { catchError, map, tap, } from 'rxjs/operators';
+import {catchError, map, tap} from 'rxjs/operators';
 
-import { ReplaySubject, Subject , Observable, of,throwError ,BehaviorSubject }    from 'rxjs';
+import {ReplaySubject, Subject, Observable, of, throwError, BehaviorSubject} from 'rxjs';
 
 import {esriMapData} from './esri-map';
-
+import {ApiService} from './api.service';
 
 declare var palette: any;
-
 
 
 @Injectable({
@@ -18,34 +17,55 @@ declare var palette: any;
 })
 export class EsriMapService {
 
-  private url ='http://192.168.34.39:8047/mapa/tematico';
+  private url = 'http://192.168.34.39:8047/mapa/tematico';
 
   private proyecto = 'frecuencias';
-  private anio ='P01';
-  private variable='P010100';
-  private ambito=0;
-  private tipoValor=1;
+  private anio = 2011;
+  private variable = 'P010100';
+  private ambito = 0;
+  private tipoValor = 1;
 
   private ambitoSource = new BehaviorSubject(0);
   private anioSource = new BehaviorSubject(2018);
   private variableSource = new BehaviorSubject('P010100');
   private clickBtnResetPeruSource = new BehaviorSubject(0);
-  
+
   private esriMapDataSource = new BehaviorSubject<any>(0);
-  private esriMapDataSources = new BehaviorSubject<esriMapData[]>([]);
-  private colorGris ='#9c9c9c';
-  private colores=[];
+  private esriMapDataSources = new BehaviorSubject<any[]>([]);
+  private colorGris = '#9c9c9c';
+  private colores = [];
+  private group = 'ccdd';
 
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient , private dataMapService: ApiService) {
     this.ambitoSource.next(0);
-    this.obtenerDatosMapaTematico().subscribe(res=>{
-      this.setEsriMapDataSource(res);
+    this.obtenerDatosMapaTematico().subscribe(res => {
+      //this.setEsriMapDataSource(res);
     });
+    /*
+    this.dataMapService.loadedData$.subscribe(
+      response => {
+        var res={};
+        var datos,rangos,arrayData=[];
+        this.colores=this.getColores(0,5);
+        datos=this.formatearDato(response);
+        datos.forEach(e => {
+          arrayData.push(e.valor);
+        });
+        rangos=this.crearRangos(arrayData,5);
+        var datosx=this.getColorPorDato(datos,rangos,this.colores);
+        res['id']=this.ambito;
+        res['rangos']=rangos;
+        res['colores']=this.colores;
+        res['data']=datosx;
+        this.setEsriMapDataSource(res);
+      }
+    );*/
+
+
   }
 
 
-  
   private handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
       // A client-side or network error occurred. Handle it accordingly.
@@ -62,27 +82,25 @@ export class EsriMapService {
       'Something bad happened; please try again later.');
   };
 
-  getColorPorDato(data,rangos,colores){
-    var datax=[];
-    var valor = undefined;
-    var codigo = '';
-    var j = 0;
-    var color = '';
+  getColorPorDato(data, rangos, colores) {
+    let datax = [];
+    let valor;
+    let codigo = '';
+    let j = 0;
+    let color = '';
 
-    data.forEach(el=>{
+    data.forEach(el => {
       codigo = el.codigo;
       valor = parseFloat(el.valor);
-      if (!(valor==null || valor==undefined)) {
+      if (!(valor == null || valor == undefined)) {
         for (j = 0; j < rangos.length; j++) {
-            if (rangos[j].min_valor <= valor && rangos[j].max_valor >= valor) {
-                color = colores[j].color;
-                el['estrato'] = j;
-                el['color'] = color;
-            }
+          if (rangos[j].min_valor <= valor && rangos[j].max_valor >= valor) {
+            color = colores[j].color;
+            el['estrato'] = j;
+            el['color'] = color;
+          }
         }
-      }
-
-      else{
+      } else {
         el['estrato'] = -1;
         el['color'] = this.colorGris;
       }
@@ -90,199 +108,202 @@ export class EsriMapService {
     });
     return datax;
   }
-  getRandomInt(min,max):number {
-    var minx = Math.ceil(min);
-    var maxx = Math.floor(max);
+
+  getRandomInt(min, max): number {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+  }
 
 
-  getColores(index,cantNiveles){
-    var listScheme=[];
-    palette.listSchemes('cb-sequential').forEach(function(e){listScheme.push(e.scheme_name)});
-    var i=this.getRandomInt(0,listScheme.length-1);
-    var p = palette(listScheme[index],cantNiveles);
-    return p.map(function (obj,index) {
-        var rObj={color:'#'+obj,id:(index),nro_break:(index)}
-        return rObj;
-    } );
+  getColores(index, cantNiveles) {
+    var listScheme = [];
+    palette.listSchemes('cb-sequential').forEach(function(e) {
+      listScheme.push(e.scheme_name);
+    });
+    var i = this.getRandomInt(0, listScheme.length - 1);
+    var p = palette(listScheme[index], cantNiveles);
+    return p.map(function(obj, index) {
+      var rObj = {color: '#' + obj, id: (index), nro_break: (index)};
+      return rObj;
+    });
 
   }
 
-  crearRangos(arrayData,cantNiveles: number){
-    arrayData=arrayData.sort(function (a,b) {return a-b;});
-    var rangos=[];
-    var i=0;
-    var el= {'min_valor':0,'max_valor':0,'label':''};;
-    var index_acu=0;
-    var index_min=0;
-    var index_max=0;
-    var cant:number=arrayData.length;
-    var coeficiente=Math.trunc(cant/cantNiveles);
-    var modulo=cant%cantNiveles;
-    var max= arrayData[cant-1];
+  crearRangos(arrayData, cantNiveles: number) {
+    arrayData = arrayData.sort(function(a, b) {
+      return a - b;
+    });
+    var rangos = [];
+    var i = 0;
+    var el = {'min_valor': 0, 'max_valor': 0, 'label': ''};
+    var index_acu = 0;
+    var index_min = 0;
+    var index_max = 0;
+    var cant: number = arrayData.length;
+    var coeficiente = Math.trunc(cant / cantNiveles);
+    var modulo = cant % cantNiveles;
+    var max = arrayData[cant - 1];
 
-    for (i=0;i<cantNiveles;i++){
-        el= {'min_valor':0,'max_valor':0,'label':''};
-        index_min=index_acu;
-        if(modulo>0) {
-            index_acu=index_acu+(coeficiente+1);
-            modulo--;
-        }
-        else{
-            index_acu=index_acu+coeficiente;
-        }
-        index_max=index_acu;
-        el.min_valor=arrayData[index_min];
+    for (i = 0; i < cantNiveles; i++) {
+      el = {'min_valor': 0, 'max_valor': 0, 'label': ''};
+      index_min = index_acu;
+      if (modulo > 0) {
+        index_acu = index_acu + (coeficiente + 1);
+        modulo--;
+      } else {
+        index_acu = index_acu + coeficiente;
+      }
+      index_max = index_acu;
+      el.min_valor = arrayData[index_min];
 
-        if(!(i==(cantNiveles-1)))
-            el.max_valor=arrayData[index_max];
-        else
-            el.max_valor=max;
-        el.label='De '+ String(el.min_valor) +' a '+ String(el.max_valor);
-        rangos.push(el);
+      if (!(i == (cantNiveles - 1))) {
+        el.max_valor = arrayData[index_max];
+      } else {
+        el.max_valor = max;
+      }
+      el.label = 'De ' + String(el.min_valor) + ' a ' + String(el.max_valor);
+      rangos.push(el);
     }
     return rangos;
 
   }
 
 
-  getEsriMapData(proyecto,version,variable,ambito,tipoValor) {
-
+  getEsriMapData(proyecto, version, variable, ambito, tipoValor) {
 
 
     const url = `${this.url}/${proyecto}/${version}/${variable}/${ambito}/${tipoValor}/`;
     return this.http.get<esriMapData>(url).pipe(
-      tap(response => {	
-       
-        
-      }),	
-      catchError(this.handleError)
-    ); 
-  }
-
-    
-    obtenerDatosMapaTematico(){
-
-      const url = `${this.url}/${this.proyecto}/${this.anio}/${this.variable}/${this.ambito}/${this.tipoValor}/`;
-      return this.http.get<esriMapData>(url).pipe(
-        tap(response => {	
-          
-          this.colores=this.getColores(0,5);
-          response.id=this.ambito;
-          var arrayData=response.data.map(x=>x.valor);
-          var rangos=this.crearRangos(arrayData,5);
-          response.rangos=rangos;
-          response.colores=this.colores;
-          response.data=this.getColorPorDato(response.data,response.rangos,response.colores);
-
-        }),	
-        catchError(this.handleError)
-      );  
-  }
+      tap(response => {
 
 
-/*
-  obtenerDatosMapaTematico(){
-
-    
-    
-    const url =`http://192.168.34.16:8877/poccrim/delitos/?groupby=ccdd&anio=2017`;
-
-    console.log('url>>>',url);
-    return this.http.get<esriMapData>(url).pipe(
-      map(response=>{
-        var res={};
-        var datos,rangos,arrayData=[];
-        this.colores=this.getColores(0,5);
-
-        datos=this.formatearDato(response);
-        
-        datos.forEach(e => {
-          arrayData.push(e.valor);
-        });
-      
-        rangos=this.crearRangos(arrayData,5);
-        var datosx=this.getColorPorDato(datos,rangos,this.colores);
-       
-        res['id']=this.ambito;
-        res['rangos']=rangos;
-        res['colores']=this.colores;
-        res['data']=datosx;
-        return res;
       }),
       catchError(this.handleError)
-
-
     );
-   
-  
-  }*/
-
-  formatearDato(datos){
-    var data = datos.map(x=> { return  {'codigo':x._id.codigo_mapa , 'valor': x.delitos} });
-    return data
   }
 
-  
-  getEsriMapDataSources():Observable<esriMapData[]>{
+  /*
+  obtenerDatosMapaTematico() {
+
+    const url = `${this.url}/${this.proyecto}/${this.anio}/${this.variable}/${this.ambito}/${this.tipoValor}/`;
+    console.log('url>>>',url);
+    return this.http.get<esriMapData>(url).pipe(
+      tap(response => {
+
+        this.colores = this.getColores(0, 5);
+        response.id = this.ambito;
+        var arrayData = response.data.map(x => x.valor);
+        var rangos = this.crearRangos(arrayData, 5);
+        response.rangos = rangos;
+        response.colores = this.colores;
+        response.data = this.getColorPorDato(response.data, response.rangos, response.colores);
+        console.log(response);
+        this.esriMapDataSource.next(response);
+
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  */
+
+    obtenerDatosMapaTematico(){
+      //const url =`http://192.168.34.16:8877/poccrim/delitos/?groupby=ccdd,ccpp&anio=2017`;
+      const url =`http://192.168.34.16:8877/poccrim/delitos/?groupby=ccdd&anio=${this.anio}`;
+      return this.http.get<esriMapData>(url).pipe(
+        tap(response => {
+          var res={};
+          var datos,rangos,arrayData=[];
+          this.colores=this.getColores(0,5);
+          datos=this.formatearDato(response);
+          datos.forEach(e => {
+            arrayData.push(e.valor);
+          });
+          rangos=this.crearRangos(arrayData,5);
+          var datosx=this.getColorPorDato(datos,rangos,this.colores);
+          res['id']=this.ambito;
+          res['rangos']=rangos;
+          res['colores']=this.colores;
+          res['data']=datosx;
+
+          this.esriMapDataSource.next(res);
+        }),
+        catchError(this.handleError)
+
+
+      );
+
+
+    }
+
+  formatearDato(datos) {
+    var data = datos.map(x => {
+      return {'codigo': x._id.codigo_mapa, 'valor': x.delitos};
+    });
+    return data;
+  }
+
+
+  getEsriMapDataSources(): Observable<esriMapData[]> {
     return this.esriMapDataSources;
   }
 
-  
-  setEsriMapDataSources(data){
+
+  setEsriMapDataSources(data) {
     return this.esriMapDataSources.next(data);
   }
 
 
-  getEsriMapDataSource() :Observable<esriMapData> {
+  getEsriMapDataSource(): Observable<esriMapData> {
     return this.esriMapDataSource;
   }
-  
-  setEsriMapDataSource(data){
+
+  setEsriMapDataSource(data) {
     return this.esriMapDataSource.next(data);
   }
 
 
-  getAmbito():Observable<any> {
-    
+  getAmbito(): Observable<any> {
+
     return this.ambitoSource;
   }
 
-  cambiarAmbito(ambito){
-    this.ambito=ambito;
-    this.ambitoSource.next(ambito);
+  cambiarAmbito(ambito) {
+    this.ambito = ambito.id;
+    this.group = ambito.group;
+    this.ambitoSource.next(this.ambito);
+
     return ambito;
   }
 
-  getAnio():Observable<any> {
+  getAnio(): Observable<any> {
     return this.anioSource;
   }
 
-  cambiarAnio(anio){
-    this.anio=anio;
+  cambiarAnio(anio) {
+    this.anio = anio;
     this.anioSource.next(anio);
     return anio;
   }
 
-  getVariable():Observable<any>{
-    
+  getVariable(): Observable<any> {
+
     return this.variableSource;
   }
 
 
-  cambiarVariable(variable){
-    this.variable=variable;
+  cambiarVariable(variable) {
+    this.variable = variable;
     this.variableSource.next(variable);
+
     return variable;
   }
 
-  clickBtnResetPeru(value){
+  clickBtnResetPeru(value) {
     this.clickBtnResetPeruSource.next(value);
   }
 
-  getBtnResetPeruSource():Observable<any>{
-    return this.clickBtnResetPeruSource
+  getBtnResetPeruSource(): Observable<any> {
+    return this.clickBtnResetPeruSource;
   }
 
 }
--- src/app/esri-map/esri-map.component.ts
+++ src/app/esri-map/esri-map.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { loadModules } from 'esri-loader';
+import {Component, OnInit, ViewChild, ElementRef} from '@angular/core';
+import {loadModules} from 'esri-loader';
 import {EsriMapService} from '../esri-map.service';
+import {ApiService} from '../api.service';
+import {Subscription} from 'rxjs';
 //import * as esri from 'esri';
 declare var palette: any;
 
@@ -15,11 +17,11 @@ export class EsriMapComponent implements OnInit {
 
   @ViewChild('mapViewNode') private mapViewEl: ElementRef;
   private zoom = 6;
-  private center: Array<number> =[-75, -9.305];
+  private center: Array<number> = [-75, -9.305];
 
   private basemap = 'streets';
   private loaded = false;
-  private map : any;
+  private map: any;
   private showLabels = true;
   private optionsApi = {
     url: 'https://js.arcgis.com/3.25/'
@@ -27,77 +29,102 @@ export class EsriMapComponent implements OnInit {
   };
 
   private proyecto = 'indicador';
-  private version ='P01';
-  private variable='P010100';
-  private ambitoInicial=0;
-  private tipoValor=1;
-  private ambitos=[2,1,0];
-  private colorGris ='#9c9c9c';
+  private version = 'P01';
+  private variable = 'P010100';
+  private anio = 2011;
+  private ambitoInicial = 0;
+  private tipoValor = 1;
+  private ambitos = [2, 1, 0];
+  private colorGris = '#9c9c9c';
   private urlServiceMap = 'https://datacrim.inei.gob.pe/mapa/arcgis/rest/services/CARTOGRAFIA_BASE_INEI/LIMITE_TEMATICOS/MapServer/';
-  private capasTematicos=[];
-  private ambito=0;
-  private colores=[
-    {id:0 , color:'#FFF9D1'},
-    {id:1,color:'#FFF16E'},
-    {id:2,color:'#FAB700'},
-    {id:3,color:'#ED7203'},
-    {id:4,color:'#E20613'},
+  private capasTematicos = [];
+  private ambito = 0;
+  private colores = [
+    {id: 0, color: '#FFF9D1'},
+    {id: 1, color: '#FFF16E'},
+    {id: 2, color: '#FAB700'},
+    {id: 3, color: '#ED7203'},
+    {id: 4, color: '#E20613'},
   ];
 
-  private listPaletas=[];
+  private listPaletas = [];
 
-  constructor(private esriMapService: EsriMapService ){
+  dataSubs: Subscription;
+
+  constructor(
+    private esriMapService: EsriMapService
+    //private dataMapService: ApiService,
+  ) {
+
+/*
+    this.dataSubs = this.dataMapService.loadedData$.subscribe(
+      res => {
+        let parsed = {};
+        parsed['data'] = res.filter(x => x._id['Año'] == '2018').map(x => {
+          return {codigo: x._id.codigo_mapa, color: '#009ae0'};
+        });
+        console.log('data_map', parsed);
+        this.actualizarCapaTematico(parsed, this.ambito);
+      }
+    );
+*/
+
+    /*    this.esriMapService.getEsriMapDataSource().subscribe(res => {
+          console.log('res map>>>',res);
+          this.actualizarCapaTematico(res, this.ambito).then(_ => {
+            //this.cambiarAmbito(this.ambito);
+          });
+        });
+    */
 
     //this.colores=this.getColores(0,5);
   }
 
 
-  async inicializarMapa(){
-    try{
-      const[Map] = await loadModules(['esri/map'],this.optionsApi);
+  async inicializarMapa() {
+    try {
+      const [Map] = await loadModules(['esri/map'], this.optionsApi);
       let mapOptions = {
         basemap: this.basemap,  // For full list of pre-defined basemaps, navigate to http://arcg.is/1JVo6Wd
         center: this.center, // longitude, latitude
         zoom: this.zoom,
-        showLabels:this.showLabels,
+        showLabels: this.showLabels,
 
       };
       this.map = new Map(this.mapViewEl.nativeElement, mapOptions);
 
-    }
-    catch(error ){
+    } catch (error) {
       console.log('EsriLoader: ', error);
 
     }
   }
 
 
-  async addCapa(url,index){
-    try{
-      var capa ={};
-      const[FeatureLayer] = await loadModules(['esri/layers/FeatureLayer'],this.optionsApi);
-      var urlFeature= `${url}/${index}`;
+  async addCapa(url, index) {
+    try {
+      var capa = {};
+      const [FeatureLayer] = await loadModules(['esri/layers/FeatureLayer'], this.optionsApi);
+      var urlFeature = `${url}/${index}`;
       var visible = false;
-      (this.ambitoInicial==index)?visible=true:false;
+      (this.ambitoInicial == index) ? visible = true : false;
 
-      var layer = new FeatureLayer (urlFeature,{
-        maxScale:0,
-        minScale:0,
+      var layer = new FeatureLayer(urlFeature, {
+        maxScale: 0,
+        minScale: 0,
         showLabels: false,
-        visible:true
+        visible: true
 
       });
 
-      capa = {layer:layer,id:index}
+      capa = {layer: layer, id: index};
 
-      this.map.addLayer(layer)
+      this.map.addLayer(layer);
       this.capasTematicos.push(capa);
 
 
-      }
-      catch(error){
-        console.log('EsriLoader: ', error);
-      }
+    } catch (error) {
+      console.log('EsriLoader: ', error);
+    }
 
   }
 
@@ -124,51 +151,47 @@ export class EsriMapComponent implements OnInit {
   }*/
 
 
-  async actualizarCapaTematico(res,index){
-    try{
-      const[Color,SimpleFillSymbol, SimpleLineSymbol,UniqueValueRenderer] = await loadModules(["esri/Color",
-      "esri/symbols/SimpleFillSymbol", "esri/symbols/SimpleLineSymbol",
-      "esri/renderers/UniqueValueRenderer"],this.optionsApi);
+  async actualizarCapaTematico(res, index) {
+    try {
+      const [Color, SimpleFillSymbol, SimpleLineSymbol, UniqueValueRenderer] = await loadModules(['esri/Color',
+        'esri/symbols/SimpleFillSymbol', 'esri/symbols/SimpleLineSymbol',
+        'esri/renderers/UniqueValueRenderer'], this.optionsApi);
 
 
       var uniqueValueInfos = res.data.map(e => {
         return {
           value: e.codigo,
-          symbol:new SimpleFillSymbol(
-            "solid"
+          symbol: new SimpleFillSymbol(
+            'solid'
             , new SimpleLineSymbol().setWidth(1)
             , new Color(e.color))
         };
       });
 
       var layerRenderer = new UniqueValueRenderer({
-        "type": "uniqueValue",
-        "field1": "CODIGO",
-        "uniqueValueInfos": uniqueValueInfos,
+        'type': 'uniqueValue',
+        'field1': 'CODIGO',
+        'uniqueValueInfos': uniqueValueInfos,
       });
 
-      var capa=this.capasTematicos.find(x=>x.id==index)
-      capa['datos']=res;
+      var capa = this.capasTematicos.find(x => x.id == index);
+      capa['datos'] = res;
       capa.layer.setRenderer(layerRenderer);
       capa.layer.redraw();
 
 
-    }
-
-    catch(error)  {
+    } catch (error) {
       console.log('EsriLoader: ', error);
     }
   }
 
-  cambiarAmbito(ambito){
+  cambiarAmbito(ambito) {
     this.capasTematicos.forEach(capa => {
-      if(capa.id==ambito){
+      if (capa.id == ambito) {
         capa.layer.setVisibility(true);
         capa.layer.setMaxScale(0);
         capa.layer.setMinScale(0);
-      }
-
-      else{
+      } else {
         capa.layer.setVisibility(false);
       }
     });
@@ -176,68 +199,52 @@ export class EsriMapComponent implements OnInit {
 
 
   public ngOnInit() {
-    //console.log('coloresss>>>',this.colores);
-    this.inicializarMapa().then(_=>{
-      //var resultados=[];
+
+    this.inicializarMapa().then(_ => {
       this.ambitos.forEach(a => {
 
-        //this.esriMapService.getEsriMapData(this.proyecto,this.version,this.variable,a,this.tipoValor).subscribe(res=>{
-            this.addCapa(this.urlServiceMap,a).then(_=>{
-              /*this.esriMapService.obtenerDatosMapaTematico().subscribe(res=>{
-                this.actualizarCapaTematico(res,a);
-              });*/
-
-
-              //if(a==this.ambitoInicial){
-
-                /*this.esriMapService.obtenerDatosMapaTematico().subscribe(res=>{
-                  this.actualizarCapaTematico(res,a);
-                  this.esriMapService.setEsriMapDataSource(res);
-                });
-*/
-
-
-              //}
-
-
+        this.addCapa(this.urlServiceMap, a).then(_ => {
+          if (a === 0) {
+            this.esriMapService.obtenerDatosMapaTematico().subscribe(res => {
             });
-
+          }
+        });
 
       });
 
+      this.esriMapService.getAmbito().subscribe(ambito => {
+          this.ambito = ambito;
+          this.esriMapService.obtenerDatosMapaTematico().subscribe(res => {
+          });
 
-      this.esriMapService.getAmbito().subscribe( ambito=>{
-            this.ambito=ambito;
-            this.esriMapService.obtenerDatosMapaTematico().subscribe(res=>{
-                console.log('res mapa ambito>>>>',res);
-                this.esriMapService.setEsriMapDataSource(res);
-                this.actualizarCapaTematico(res,this.ambito).then(_=>{
-                  this.cambiarAmbito(this.ambito);
-                });
-
-            });
-         }
+        }
       );
 
 
-
-      this.esriMapService.getVariable().subscribe( variable=>{
-
-
-        this.variable=variable;
-        this.esriMapService.obtenerDatosMapaTematico().subscribe(res=>{
-            console.log('res mapa>>>> variable',res);
-            this.esriMapService.setEsriMapDataSource(res);
-            this.actualizarCapaTematico(res,this.ambito);
+      /*this.esriMapService.getVariable().subscribe(variable => {
+        this.variable = variable;
+        this.esriMapService.obtenerDatosMapaTematico().subscribe(res => {
         });
-     });
+      });*/
+
+      this.esriMapService.getAnio().subscribe(anio => {
+        this.anio = anio;
+        this.esriMapService.obtenerDatosMapaTematico().subscribe(res => {
+        });
+      });
 
 
+      this.esriMapService.getBtnResetPeruSource().subscribe(res => {
+        this.map.centerAndZoom(this.center, this.zoom);
+      });
 
-     this.esriMapService.getBtnResetPeruSource().subscribe(res=>{
-       console.log('res boton>>>',res);
-       this.map.centerAndZoom(this.center, this.zoom);
-     });
+
+      this.esriMapService.getEsriMapDataSource().subscribe(res => {
+        this.actualizarCapaTematico(res, this.ambito).then(_ => {
+          this.cambiarAmbito(this.ambito);
+        });
+      });
+
 
       /*this.esriMapService.getAmbito().subscribe(res=>{
         var ambito = res;
